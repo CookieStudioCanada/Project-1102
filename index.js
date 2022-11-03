339aa61d0b82f2157f3ee5897d2b55a2d6174c9c
Peter <pierre-marc.seers@hotmail.com>
2022-11-03 19:28:53 -0400

    Test 3 nov. 7h30

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ let divrepEl = document.getElementById("div-rep-el")
 let divnonrepEl = document.getElementById("div-nonrep-el")
 let pdajustEl = document.getElementById("pd-ajust-el")
 let impot552El = document.getElementById("impot-552-el")
+let cdc552El = document.getElementById("cdc-552-el")
+let imrtd552El = document.getElementById("imrtd-552-el")
+
+// GC data
+let impotEl = document.getElementById("impot-el")
 let cdcEl = document.getElementById("cdc-el")
 let imrtdEl = document.getElementById("imrtd-el")
 
@@ -29,15 +34,53 @@ function Button552()
     let cdc = gci
     let imrtd = gci * 0.30666666
 
-    console.log("gc : " + gc)
-    console.log("gci : " + gci)
-    console.log("impot : " + impot)
-
     divrepEl.textContent = divrep + " $"
     divnonrepEl.textContent = divnonRP + " $"
     pdajustEl.textContent = pdajust + " $"
     impot552El.textContent = impot + " $"
-    cdcEl.textContent = cdc + " $"
-    imrtdEl.textContent = imrtd + " $"
+    cdc552El.textContent = cdc + " $"
+    imrtd552El.textContent = imrtd + " $"
     
 }
+
+// function de GC
+
+function ButtonGC()
+{
+    let pd = document.getElementById("PD").value
+    let pbr = document.getElementById("PBR").value
+    
+    let gc = pd - pbr
+    let gci = gc / 2
+    let impot = gci * 0.5017
+
+    let cdc = gci
+    let imrtd = gci * 0.30666666
+
+    impotEl.textContent = impot + " $"
+    cdcEl.textContent = cdc + " $"
+    imrtdEl.textContent = imrtd + " $"
+}
+
+// tabs
+
+function tabs(evt, sections) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(sections).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
